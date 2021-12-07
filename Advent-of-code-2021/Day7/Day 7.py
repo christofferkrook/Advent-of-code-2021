7e5c096e3d3c07b2721c5aa12f74c47d0238860a
christofferkrook <93980808+christofferkrook@users.noreply.github.com>
2021-12-07 08:00:00 +0100

    day 7
    day 7

---
+++ Advent-of-code-2021/Day7/Day 7.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 07:30:59 2021
+
+@author: chk
+"""
+
+import os
+import numpy as np
+
+def main():
+    # part 1
+    os.chdir('C:/Users/chk/Documents/GitHub/Advent-of-code-2021/Advent-of-code-2021/Day7')
+    f = open("input.txt")
+    positions = str.split(f.readline(), ',')
+    
+    for i in range(len(positions)):
+        positions[i] = int(positions[i])
+    
+   #smallestFuelPos = np.round(sum(positions)/len(positions))
+    smallestFuelPos = np.round(np.median(positions))
+    
+    fuel = 0
+    for x in positions:
+        fuel += np.abs(smallestFuelPos - x)
+    
+    print(int(fuel))
+    
+    
+    # part 2
+    os.chdir('C:/Users/chk/Documents/GitHub/Advent-of-code-2021/Advent-of-code-2021/Day7')
+    f = open("input.txt")
+    positions = str.split(f.readline(), ',')
+    
+    for i in range(len(positions)):
+        positions[i] = int(positions[i])
+    
+    smallestFuelPos = np.round(sum(positions)/len(positions))
+    
+    temp = 0
+    for x in positions:
+        temp += (sum(list(range(1, int(np.abs(x-smallestFuelPos)) + 1))))
+
+    print(temp)
+   
+    
+if __name__ == '__main__':
+    main()
